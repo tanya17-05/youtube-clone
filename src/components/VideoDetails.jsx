46223421042d6a4a4d4f45e166c8dd4bb7475557
Tanya <tanyakumari332@gmail.com>
2023-04-25 17:13:53 +0530

    no scroll in in suggestion

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -114,7 +114,7 @@ const VideoDetails = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]">
+        <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px] no-scroll-bar">
           {relatedVideos?.contents?.map((item, index) => {
             if (item.type !== "video") return false;
             return <SuggestionVideoCard key={index} video={item.video} />;
